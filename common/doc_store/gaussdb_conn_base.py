a5da06d091228494c0f7d66244fa15c9b1669e7e
Seven <seven1024280171@163.com>
2026-07-09 16:42:29 +0800

    feat: implement gaussdb chunk crud

--- common/doc_store/gaussdb_conn_base.py
+++ common/doc_store/gaussdb_conn_base.py
@@ -241,6 +241,8 @@ class GaussDBConnectionBase(DocStoreConnection):
         self.pool = pool
         self.masked_uri = self.pool.masked_uri
         self.resolved_schema = self.pool.resolved_schema
+        self.schema = self.resolved_schema
+        self.ddl = GaussDBDDLBuilder(schema=self.resolved_schema)
         self.pool.check_schema_access()
         self.logger.info("GaussDB %s connection initialized.", self.masked_uri)
 
